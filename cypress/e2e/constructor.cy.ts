197cecc2f32932e1b7f867e9147dc7a7c785c519
Roman Maslennikov <romzes120282@gmail.com>
2024-09-30 15:35:09 +0300

    done: 1) should add bun 2) should add ingredient

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,25 +1,25 @@
 describe('Cypress test constructor', () => {
-    beforeEach('Настройка перехвата запросов', () => {
-        cy.viewport(1300, 800);
-        cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
-        // cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
-        cy.visit('http://localhost:4000');
-    });
+  beforeEach('Настройка перехвата запросов', () => {
+    cy.viewport(1300, 800);
+    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
+    // cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
+    cy.visit('http://localhost:4000');
+  });
 
-    // добавление булки и начинки
-    it('should add bun', () => {
-        cy.get('[data-cy=buns]').contains('Добавить').click();
+  // добавление булки и начинки
+  it('should add bun', () => {
+    cy.get('[data-cy=buns]').contains('Добавить').click();
 
-        cy.get('[data-cy=bunTop]').should('contain', 'Краторная булка N-200i');
-        cy.get('[data-cy=bunBottom]').should('contain', 'Краторная булка N-200i');
-    });
+    cy.get('[data-cy=constructor-bun-top]').should('contain', 'Краторная булка N-200i');
+    cy.get('[data-cy=constructor-bun-bottom]').should('contain', 'Краторная булка N-200i');
+  });
 
-    it('should add ingredient', () => {
-        cy.get('[data-cy=mains]').contains('Добавить').click();
+  it('should add ingredient', () => {
+    cy.get('[data-cy=mains]').contains('Добавить').click();
 
-        cy.get('[data-cy=ingredientItem]').should(
-            'contain',
-            'Биокотлета из марсианской Магнолии'
-        );
-    });
-)}    
+    cy.get('[data-cy=mains-ingredients]').should(
+      'contain',
+      'Биокотлета из марсианской Магнолии'
+    );
+  });
+});
